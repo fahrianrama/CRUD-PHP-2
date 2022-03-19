1adb94e5c26389ffef79c62dbc4e8bdc79ee3663
fahrianrama <fahrianramaditiya7@gmail.com>
2022-03-19 07:38:35 +0700

    crud_2

---
+++ ilkomstudy/app/views/home/index.php
@@ -0,0 +1,189 @@
+
+<body data-spy="scroll" data-offset="80">
+
+  <!-- Preloader -->
+  <div class="animationload">
+    <div class="loader">
+        Please Wait....
+    </div>
+  </div> 
+  <!-- End Preloader -->
+
+
+  <nav class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Ilkomstudy</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="#services">Fitur</a></li>
+          <li><a href="http://localhost/ilkomstudy/public/katalog">Katalog</a></li>
+          <li><a href="http://localhost/ilkomstudy/public/login"><button type="button" class="btn btn-success">Login</button></a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+  </nav>
+
+    <!-- /HOME -->
+    <section class="main-home" id="home">
+      <div class="home-page-photo"></div> <!-- Background image -->
+      <div class="home__header-content">
+        <div id="main-home-carousel" class="owl-carousel">
+          <div>
+            <h1 class="intro-title">Start your own project</h1>
+            <p class="intro-text">Lorem ipsum dolor sit amet consectetur adictum piscing elit fusce sit amet inerdum neque ultra icies <br/>  pelentesque tempor justo a scelerisque placerat sit amet magna e.</p>
+            <a class="btn btn-custom" href="http://localhost/ilkomstudy/public/katalog">Katalog Buku</a>
+          </div><!--slide item like paragraph-->
+
+          <div>
+            <h1 class="intro-title">Best ideas for your Business</h1>
+            <p class="intro-text">Lorem ipsum dolor sit amet consectetur adictum piscing elit fusce sit amet inerdum neque ultra icies <br/>  pelentesque tempor justo a scelerisque placerat sit amet magna e.</p>
+            <a class="btn btn-custom" href="http://localhost/ilkomstudy/public/katalog">Katalog Buku</a>
+          </div><!--slide item like paragraph-->
+
+          <div>
+            <h1 class="intro-title">We are digital expert</h1>
+            <p class="intro-text">Lorem ipsum dolor sit amet consectetur adictum piscing elit fusce sit amet inerdum neque ultra icies <br/>  pelentesque tempor justo a scelerisque placerat sit amet magna e.</p>
+            <a class="btn btn-custom" href="http://localhost/ilkomstudy/public/katalog">Katalog Buku</a>
+          </div><!--slide item like paragraph-->
+        </div>
+      </div>
+    </section>
+    <!-- /End HOME -->
+
+    <!-- / SERVICES -->
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="title text-center">Fitur dan Keunggulan</h3>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-design-graphic-tablet-streamline-tablet color-l-orange"></i> 
+                </div>
+                <h5>Mudah dipakai</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-design-pencil-rule-streamline color-l-blue"></i> 
+                </div>
+                <h5>Membuat Data Baru</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-speech-streamline-talk-user color-l-yellow"></i> 
+                </div>
+                <h5>Administrasi Mudah</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+        </div> <!--/.row -->
+
+        <div class="row">
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-settings-streamline-2 color-l-purple"></i> 
+                </div>
+                <h5>Update Data</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-streamline-umbrella-weather color-l-pink"></i> 
+                </div>
+                <h5>Aman</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                  <i class="icon-caddie-shopping-streamline color-l-green"></i> 
+                </div>
+                <h5>Hapus Data</h5>
+                <p>Nulla vitae libero pharetra augue. Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+              </div>
+            </div>
+          </div>
+        </div> <!--/.row -->
+      </div> <!--/.container -->
+    </section>
+    <!-- / End services-->
+
+
+    <!-- TWITTER TWEET -->
+    <section class="twitter_tweet parallax" id="twitter_tweet" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <div id="testi-carousel" class="owl-carousel owl-spaced">
+              <div>
+                <i class="fa fa-twitter"></i>
+                <h5>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat, eligendi magni quibusdam iure cupiditate ex voluptas unde Lorem ipsum dolor sit amet..
+                </h5>
+                <p>- Jonathan Deo</p>
+              </div><!--testimonials item like paragraph-->
+              <div>
+                <i class="fa fa-twitter"></i>
+                <h5>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat, eligendi magni quibusdam iure cupiditate ex voluptas unde Lorem ipsum dolor sit amet..
+                </h5>
+                <p>- Jonathan Deo</p>
+              </div><!--testimonials item like paragraph-->
+              <div>
+                <i class="fa fa-twitter"></i>
+                <h5>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum, fugiat, eligendi magni quibusdam iure cupiditate ex voluptas unde Lorem ipsum dolor sit amet..
+                </h5>
+                <p>- Jonathan Deo</p>
+              </div><!--testimonials item like paragraph-->
+            </div>
+          </div> <!-- end col-md-8 -->
+        </div> <!-- end row -->
+      </div> <!-- container -->
+    </section>
+    <!-- End TWITTER TWEET -->
+
+
+    
